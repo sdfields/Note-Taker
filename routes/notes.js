f77ae4ac270ca70182449ec4a838e1277e32c577
sdfields <spencerdfields@gmail.com>
2023-01-18 15:07:31 -0600

    added get and post routes for notes and ability to write to array

--- routes/notes.js
+++
@@ -1,7 +0,0 @@
-// Variables and Imports
-const notes = require('express').Router();
-
-// GET Route for Notes
-notes.get('/', (req, res) => {
-    console.info(`${req.method} request received for notes.`);
-})
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Boilerplate variables
 
-const express = require('express');
-const path = require('path');
-const db = require('./db/db.json');
-const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const db = require("./db/db.json");
+const fs = require("fs");
 
 const PORT = 3001;
 
@@ -14,26 +14,55 @@ const app = express();
 // Middleware
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+app.use(express.urlencoded({ extended: true }));
 // app.use('/api', api);
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// Get route for landing page
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
+// GET request for notes
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/notes.html"))
 );
 
-// GET route for notes
-
-app.get('/notes', (req, res) => 
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
-
-// GET route for api/notes
-app.get('/api/notes', (req, res) => {
-    res.json(db)
+// GET request for api/notes (database)
+app.get("/api/notes", (req, res) => {
+  fs.readFile('./db/db.json', "utf8", (error, data) => {
+    if (error) {
+        res.json(error);
+    } else {
+        data = JSON.parse(data);
+        res.json(data);
+    }
+  }
+  )
 });
 
+// POST request for notes
+
+app.post("/api/notes", (req, res) => {
+  const newNote = req.body;
+  fs.readFile("./db/db.json", "utf8", (error, data) => {
+    if (error) {
+      res.json(error);
+    } else {
+        data = JSON.parse(data);
+      data.push(newNote);
+      fs.writeFile("./db/db.json", JSON.stringify(data), (error) => {
+        if (error) {
+          res.json(error);
+        } else {
+          res.json("Note added!");
+        }
+      });
+    }
+  });
+});
+
+// GET request for landing page
+app.get("*", (req, res) =>
+  res.sendFile(path.join(__dirname, "/public/index.html"))
+);
+
 app.listen(PORT, () =>
-    console.log(`Server listening at http://localhost:${PORT}`)
+  console.log(`Server listening at http://localhost:${PORT}`)
 );
