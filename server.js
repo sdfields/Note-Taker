58755eaa5c84c9b34ee3780e8b8ea194f1563cd3
sdfields <spencerdfields@gmail.com>
2023-01-18 22:36:56 -0600

    added possible delete request

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 // Boilerplate variables
 
-const express = require("express");
-const path = require("path");
-const db = require("./db/db.json");
-const fs = require("fs");
+const express = require('express');
+const path = require('path');
+const db = require('./db/db.json');
+const fs = require('fs');
 
 const PORT = 3001;
 
@@ -16,53 +16,75 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 // app.use('/api', api);
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // GET request for notes
 
-app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/notes.html"))
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
 // GET request for api/notes (database)
-app.get("/api/notes", (req, res) => {
-  fs.readFile('./db/db.json', "utf8", (error, data) => {
+app.get('/api/notes', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (error, data) => {
     if (error) {
-        res.json(error);
+      res.json(error);
     } else {
-        data = JSON.parse(data);
-        res.json(data);
+      data = JSON.parse(data);
+      res.json(data);
     }
-  }
-  )
+  });
 });
 
 // POST request for notes
 
-app.post("/api/notes", (req, res) => {
+app.post('/api/notes', (req, res) => {
   const newNote = req.body;
-  fs.readFile("./db/db.json", "utf8", (error, data) => {
+  fs.readFile('./db/db.json', 'utf8', (error, data) => {
     if (error) {
       res.json(error);
     } else {
-        data = JSON.parse(data);
+      data = JSON.parse(data);
+      newNote.id = Math.floor(Math.random()*1000)
       data.push(newNote);
-      fs.writeFile("./db/db.json", JSON.stringify(data), (error) => {
+      fs.writeFile('./db/db.json', JSON.stringify(data), (error) => {
         if (error) {
           res.json(error);
         } else {
-          res.json("Note added!");
+          res.json('Note added!');
         }
       });
     }
   });
 });
 
+// Delete request for notes
+
+app.delete('/api/notes/:id', (req, res) => {
+  fs.readFile('./db/db.json', 'utf8', (error, data) => {
+    if (error) {
+      res.json(error)
+    } else {
+      res.json('Note deleted!')
+    }
+    let notes = JSON.parse(data)
+    let newNotes = notes.filter(note => note.id !== parseInt(req.params.id));
+    fs.writeFile('./db/db.json', JSON.stringify(newNotes), (error) => {
+      if (error) {
+        res.json(error);
+      } else {
+        res.json('Note deleted!');
+      }
+    });
+  })
+});
+
 // GET request for landing page
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "/public/index.html"))
+app.get('*', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
+// Listen to PORT variable
 app.listen(PORT, () =>
   console.log(`Server listening at http://localhost:${PORT}`)
 );
